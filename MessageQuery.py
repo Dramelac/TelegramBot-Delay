b227fc64b3b5bfa2bdcd5ebaa8b733159a1301b2
Dramelac <203697@supinfo.com>
2018-11-07 12:15:05 +0100

    Group improvement

--- MessageQuery.py
+++ MessageQuery.py
@@ -8,14 +8,21 @@ class MessageQuery:
         self.bot = bot
         self.debug_msg = msg
         self.is_edit = False
-        self.is_inline = None
+        self.is_inline = False
+        self.is_group = False
+        self.new_users = None
+        self.group_name = None
+
+        pprint(msg)
 
         if msg.get("message") is not None:
             msg = msg["message"]
-            self.is_inline = False
+            if msg.get("new_chat_member"):
+                self.new_users = msg["new_chat_members"]
+            elif msg.get("left_chat_member"):
+                print("[DEBUG] Group member leaving is not supported")
         elif msg.get("edited_message") is not None:
             msg = msg["edited_message"]
-            self.is_inline = False
             self.is_edit = True
         elif msg.get("inline_query"):
             msg = msg["inline_query"]
@@ -32,7 +39,11 @@ class MessageQuery:
             self.document = msg.get("document")
             self.animation = msg.get("animation")
             self.photo = msg.get("photo")
+            self.reply = True if msg.get("reply_to_message") is not None else False
             self.chat_id = msg["chat"]["id"]
+            self.is_group = True if msg.get("chat") is not None and msg.get("chat").get("type") == "group" else False
+            if self.is_group:
+                self.group_name = msg.get("chat").get("title")
 
         self.username = msg["from"]["first_name"]
         self.lang = msg["from"].get("language_code")
@@ -44,6 +55,16 @@ class MessageQuery:
     def handle(self):
         if self.is_edit:
             return "Command editing is not yet supported :'(", self.chat_id
+        if self.reply:
+            if self.is_group:
+                return None
+            return "Response is not yet supported :'(", self.chat_id
+
+        # Group custom
+        if self.is_group:
+            ret = self.__handle_group_specific()
+            if ret is not None:
+                return ret, self.chat_id
 
         check = self.__type_error()
         if check is not None:
@@ -88,15 +109,35 @@ class MessageQuery:
                 return self.__print_help_fr()
             else:
                 return self.__print_help_en()
-        elif self.text == "ping":
-            return "pong"
+        elif re.match(r'^[/\\]?[Pp][Ii][Nn][Gg]$', self.text):
+            return "Pong"
         elif self.__hello_match() is not None:
             return self.__hello_match()
         else:
             return self.__handle_query()
 
+    def __handle_group_specific(self):
+        if self.new_users is not None:
+            users_name = ""
+            for user in self.new_users:
+                # Detect self team joining
+                if user.get("id") == self.bot.id:
+                    if self.lang == 'fr-FR' or self.lang == "fr":
+                        return self.__print_help_fr()
+                    else:
+                        return self.__print_help_en()
+                else:
+                    users_name += user.get("username") + " "
+            if users_name != "":
+                if self.lang == 'fr-FR' or self.lang == "fr":
+                    return self.__welcome_message_fr(users_name)
+                else:
+                    return self.__welcome_message_en(users_name)
+
+        return None
+
     def __handle_query(self):
-        rule = r"(\d+)\s?(\w+)\s(.+)"
+        rule = r'^[/\\]?[ ]?(\d+)\s?(\w+)\s(.+)'
         reout = re.match(rule, self.text)
         if reout is None:
             return self.__get_syntax_error()
@@ -132,11 +173,11 @@ class MessageQuery:
         return "Query saved !"
 
     def __hello_match(self):
-        if re.match(r'^([hH][ea]llo|[hH]i|[gG]ood|[Hh]ey)( ?.*)?', self.text):
+        if re.match(r'^[/\\]?([hH][ea]llo|[hH]i|[gG]ood|[Hh]ey)( ?.*)?', self.text):
             resp = "Hello " + self.username
             resp += " !" if re.match(r'.*!$', self.text) else ""
             return resp
-        elif re.match(r'^([sS]alut|[cC](ou)?[cC](ou)?|[bB]onjour|[Yy][oO]p?)( ?.*)?', self.text):
+        elif re.match(r'^[/\\]?([sS]alut|[cC](ou)?[cC](ou)?|[bB]onjour|[Yy][oO]p?)( ?.*)?', self.text):
             resp = "Bonjour " + self.username
             resp += " !" if re.match(r'.*!$', self.text) else ""
             return resp
@@ -144,20 +185,38 @@ class MessageQuery:
             return None
 
     def __print_help_fr(self):
-        return "Bonjour " + self.username + " !\n" \
+        if self.is_group:
+            target = "à tous"
+            prefix = "/"
+        else:
+            target = self.username
+            prefix = ""
+        return "Bonjour " + target + " !\n" \
                                             "Je suis @MrDelayBot !\n" \
                                             "Je peux vous envoyer des messages dans le futur !\n" \
                                             "Syntax : \n" \
-                                            "   \"10s ce message me sera envoyé dans 10 secondes\"\n" \
-                                            "   \"1j ce message me sera envoyé dans 1 jour\""
+                                            "   \"" + prefix + "10s ce message me sera envoyé dans 10 secondes\"\n" \
+                                            "   \"" + prefix + "1j ce message me sera envoyé dans 1 jour\""
 
     def __print_help_en(self):
-        return "Hello " + self.username + " !\n" \
+        if self.is_group:
+            target = "everyone"
+            prefix = "/"
+        else:
+            target = self.username
+            prefix = ""
+        return "Hello " + target + " !\n" \
                                           "I'm @MrDelayBot !\n" \
                                           "I can send you message in the futur !\n" \
                                           "Syntax : \n" \
-                                          "   \"10s this message will be sent to you in 10 seconds\"\n" \
-                                          "   \"1d this message will be sent to you in 1 day\""
+                                          "   \"" + prefix + "10s this message will be sent to you in 10 seconds\"\n" \
+                                          "   \"" + prefix + "1d this message will be sent to you in 1 day\""
+
+    def __welcome_message_fr(self, users):
+        return "Bonjour " + users + "bienvenue sur " + self.group_name + " !"
+
+    def __welcome_message_en(self, users):
+        return "Hello " + users + "welcome on " + self.group_name + " !"
 
     @staticmethod
     def __get_syntax_error():
