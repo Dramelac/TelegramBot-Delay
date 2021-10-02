fd9442129dee964fbf0da01767f6f6d56e486e52
Dramelac <203697@supinfo.com>
2021-10-02 15:57:09 +0200

    Add up delay

--- MessageQuery.py
+++ MessageQuery.py
@@ -12,6 +12,7 @@ class MessageQuery:
         self.is_edit = False
         self.is_inline = False
         self.is_group = False
+        self.is_reply = is_reply
         self.group_operation = False
         self.new_users = None
         self.group_name = None
@@ -29,6 +30,8 @@ class MessageQuery:
             msg = msg["inline_query"]
             self.text = msg["inline_query"]["query"]
             self.is_inline = True
+        elif self.is_reply:
+            self.text = msg.get('text')
         else:
             Logger.g().error("Message type not supported\n", pformat(msg))
             return
@@ -59,9 +62,6 @@ class MessageQuery:
         response_message = None
         if self.is_edit:
             response_message = "Message editing is not supported :'("
-        elif self.reply:
-            if not self.is_group:
-                response_message = "Response is not yet supported :'("
         # Group custom
         elif self.is_group:
             response_message = self.__handle_group_specific()
@@ -104,6 +104,8 @@ class MessageQuery:
         return None
 
     def __handle_message(self):
+        if self.reply and not self.reply_message.__is_reply_message_valid():
+            return None
         # Send Hello message
         if re.match(r'^[/]?start$', self.text, re.IGNORECASE) or re.match(r'^[/]?help$', self.text, re.IGNORECASE):
             if self.lang == 'fr-FR' or self.lang == "fr":
@@ -114,8 +116,43 @@ class MessageQuery:
             return "Pong"
         elif self.__hello_match() is not None:
             return self.__hello_match()
+        elif self.reply:
+            return self.__handle_reply()
         else:
             return self.__handle_query()
+
+    def __is_reply_message_valid(self):
+        # Check if source message is from DelayBot
+        if self.is_reply:
+            from_data = self.debug_msg.get('from', {})
+            if not from_data.get('is_bot') and from_data.get('username', '') != self.bot.username:
+                return False
+            # Exclude help and error messages
+            if self.text in [self.__get_syntax_error(),
+                             self.__print_help_en(),
+                             self.__print_help_fr(),
+                             "Query saved !",
+                             "Up saved !",
+                             "Up message in 24h !"]:
+                return False
+            return True
+        return False
+
+    def __handle_reply(self):
+        if self.reply_message is not None and self.reply_message.__is_reply_message_valid():
+            msg, delay_time = self.__time_command_parser(self.text, up_mode=True)
+            if delay_time is None:
+                return msg
+
+            if self.chat_id is not None:
+                self.bot.schedule_message(self.reply_message.text, delay_time, self.chat_id)
+            else:
+                Logger.g().debug("Send ", self.reply_message.text, " delayed ", delay_time)
+                return "Debug up query"
+
+            return "Up saved !" if msg is None else msg
+
+        return None
 
     def __handle_group_specific(self):
         if self.new_users is not None:
@@ -158,27 +195,10 @@ class MessageQuery:
         return delay_time
 
     def __handle_query(self):
-        reout = re.match(r'^[/\\]?(\d+)([a-zA-Z]+)(\d+)?([a-zA-Z]+)?(\d+)?([a-zA-Z]+)?\s(.+)', self.text, re.DOTALL)
-        if reout is None:
-            return self.__get_syntax_error()
-        time_type = "unknown"
-        try:
-            delay_time = 0
-            for i in range(1, 6, 2):
-                if reout.group(i) is None:
-                    if i == 1:
-                        raise IndexError
-                    break
-                time_nb = int(reout.group(i))
-                time_type = reout.group(i + 1)
-                delay_time += MessageQuery.compute_time(time_nb, time_type)
-            msg = reout.group(7)
-        except IndexError:
-            return self.__get_syntax_error()
-        except TypeError:
-            resp = "Time type '" + time_type + "' not understood :/"
-            Logger.g().error(resp)
-            return resp
+
+        msg, delay_time = self.__time_command_parser(self.text)
+        if delay_time is None:
+            return msg
 
         if self.chat_id is not None:
             self.bot.schedule_message(msg, delay_time, self.chat_id)
@@ -187,6 +207,49 @@ class MessageQuery:
             return "Debug query"
 
         return "Query saved !"
+
+    @classmethod
+    def __time_command_parser(cls, text, up_mode=False):
+        result_time = None
+        if up_mode:
+            re_command = re.match(r'^[/\\]?(up\s)?(((\d+)([a-zA-Z]+))+)', text, re.DOTALL)
+            # Match default /up
+            if re.match(r'^[/\\]?up\s?$', text, re.DOTALL) is not None:
+                return "Up message in 24h !", 60 * 60 * 24
+            # Math custom up time '/up 1h' or '/1h'
+            elif re_command is not None:
+                return cls.__time_parser(re_command.group(2), None)
+            else:
+                return None, None  # Send nothing
+        # Old limited time set
+        # reout = re.match(r'^[/\\]?(\d+)([a-zA-Z]+)(\d+)?([a-zA-Z]+)?(\d+)?([a-zA-Z]+)?\s(.+)', text, re.DOTALL)
+        # Match command time syntax
+        re_global = re.match(r'^[/\\]?(([\d]+[a-zA-Z]+)+)\s(.+)', text, re.DOTALL)
+        if re_global is None:
+            result_text = cls.__get_syntax_error()
+        else:
+            time_command = re_global.group(1)
+            message = re_global.group(3)
+            result_text, result_time = cls.__time_parser(time_command, message)
+        return result_text, result_time
+
+    @classmethod
+    def __time_parser(cls, time_text, msg):
+        delay_time = 0
+        time_type = "unknown"
+        try:
+            # Find all time from first part of text command
+            matches = re.finditer(r"(\d+)([a-zA-Z]+)", time_text)
+            for match in matches:
+                time_nb = int(match.group(1))
+                time_type = match.group(2)
+                delay_time += MessageQuery.compute_time(time_nb, time_type)
+        except TypeError:
+            resp = "Time type '" + time_type + "' not understood :/"
+            Logger.g().error(resp)
+            msg = resp
+            delay_time = None  # Delay time must be set to none to send error message immediately
+        return msg, delay_time
 
     def __hello_match(self):
         if re.match(r'^[/]?(h[ea]llo|hi|good|hey)( ?.*)?$', self.text, re.IGNORECASE):
@@ -233,11 +296,13 @@ Here it is !
             target = self.username
             prefix = ""
         return "Bonjour {0} !\n" \
-               "Je suis @MrDelayBot !\n" \
+               "Je suis @{1} !\n" \
                "Je peux vous envoyer des messages dans le futur !\n" \
                "Syntax : \n" \
-               "   \"{1}10s ce message me sera envoyé dans 10 secondes\"\n" \
-               "   \"{1}1j2h ce message me sera envoyé dans 1 jour et 2 heures\"".format(target, prefix)
+               "   \"{2}10s ce message me sera envoyé dans 10 secondes\"\n" \
+               "   \"{2}1j2h ce message me sera envoyé dans 1 jour et 2 heures\"".format(target,
+                                                                                         self.bot.username,
+                                                                                         prefix)
 
     def __print_help_en(self):
         if self.is_group:
@@ -247,11 +312,13 @@ Here it is !
             target = self.username
             prefix = ""
         return "Hello {0} !\n" \
-               "I'm @MrDelayBot !\n" \
+               "I'm @{1} !\n" \
                "I can send you message in the futur !\n" \
                "Syntax : \n" \
-               "   \"{1}10s this message will be sent to you in 10 seconds\"\n" \
-               "   \"{1}1d2h this message will be sent to you in 1 day ans 2 hours\"".format(target, prefix)
+               "   \"{2}10s this message will be sent to you in 10 seconds\"\n" \
+               "   \"{2}1d2h this message will be sent to you in 1 day ans 2 hours\"".format(target,
+                                                                                             self.bot.username,
+                                                                                             prefix)
 
     def __welcome_message_fr(self, users):
         return "Bonjour {0}bienvenue sur {1} !".format(users, self.group_name)
